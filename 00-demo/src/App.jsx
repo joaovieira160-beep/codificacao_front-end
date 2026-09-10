83aa0907eabbc39cb15f9f965f6df38672e4de70
JOÃO PEDRO VIEIRA <joao_vieira160@estudante.sesisenai.org.br>
2026-09-10 15:39:29 -0300

    finalizaçao atividade

--- 00-demo/src/App.jsx
+++ 00-demo/src/App.jsx
@@ -24,11 +24,13 @@ function App() {
        <p>
         Setor Público: Órgãos governamentais e universidades por meio de estágios, contratos temporários ou concursos públicos para cargos técnicos.
        </p>
+       <br />
 
        <h2>Trampos com tecnologia mas sem programação</h2>
        <p>
         Gostar de tecnologia sem ter afinidade com código não é um obstáculo. O mercado de TI é gigante e depende fortemente de funções que focam em pessoas, processos, design, dados e negócios.
        </p>
+       
 
         <ul>
           <li>Product Management & Negócios</li>
@@ -37,6 +39,44 @@ function App() {
           <li>Dados e Inteligência de Negócio</li>
           <li>Vendas Técnicas e Sucesso do Cliente</li>
         </ul>
+
+        <h2>onde posso usar TI fora da TI?</h2>
+
+        <p>
+          A Tecnologia da Informação (TI) deixou de ser apenas um setor isolado e se tornou uma ferramenta estratégica essencial em praticamente todas as áreas do mercado. Incorporar TI fora de empresas de tecnologia permite automatizar processos, otimizar tomadas de decisão e criar novas experiências.
+        </p>
+        <br />
+
+        <p>
+          Sistemas de Recomendação Personalizada: Algoritmos de aprendizado de máquina (Machine Learning) em e-commerces que sugerem produtos com base no histórico de navegação e compras do cliente
+          </p>
+
+          <p>
+            Detecção de Fraudes em Tempo Real: Sistemas que analisam padrões de comportamento do usuário em transações de cartão de crédito e bloqueiam operações suspeitas em milissegundos.
+
+          </p>
+
+          <p>
+            Criação de Ferramentas Próprias: Você pode criar pequenos painéis ou calculadoras para resolver gargalos do seu dia a dia profissional sem depender do setor de TI da empresa.
+
+          </p>
+
+          <p>
+            Automação de Processos: Redução de erros humanos e ganho de tempo (ex: OCR, ERPs).
+
+          </p>
+
+          <p>
+            Contabilidade Automatizada: Leitura óptica de caracteres (OCR) para extrair dados de notas fiscais e recibos e inseri-los automaticamente em sistemas contábeis.
+          </p>
+
+          <p>
+            Empreendedorismo e Soluções Setoriais (HealthTech, LegalTech, EdTech)
+          </p>
+
+          <p>
+          Autonomia Tecnológica: Você não fica dependente da equipe de TI da empresa para resolver problemas básicos do seu dia a dia.
+          </p>
 
    </div>
   )
