420927cc7edcb8f516629486a8c04fee8b629387
JOÃO PEDRO VIEIRA <joao_vieira160@estudante.sesisenai.org.br>
2026-09-16 17:00:52 -0300

    primeiras atividades

--- 01-js/src/App.jsx
+++ 01-js/src/App.jsx
@@ -2,6 +2,130 @@ import './App.css'
 
 function App() {
 
+      function 
+
+      function PlanejarS(){
+        let bombasF = 7
+        let preco = Number(prompt("quantos reais cada bomba de fumaça custa?"))
+        let shows = Number(prompt("quanto shows estao marcados?:"))
+
+        let totalB = bombasF * shows
+        let totalValor = totalB * preco
+
+        alert(`Voce precisa de ${totalB} bombas de fumaça,o custo sera de R$${totalValor.toFixed(2)}`)
+      }
+
+
+      function mensalBarco(){
+        let suprimentos = Number(prompt("quanto foi gasto em suprimentos e mercadorias este mes?"))
+        let vendas = Number(prompt("quanto foi o faturamento de vendas de ingressos este mes?"))
+        let itens = Number(prompt("quanto foi o faturamento de vendas de itens este mes?"))
+
+        let totalfaturamento = vendas + itens
+        let lucro = totalfaturamento - suprimentos
+        let porcentagem = (lucro*100)/ suprimentos 
+
+
+        alert(`o total de seu faturamento de vendas esse mes foi de R$${lucro}\nOu seja ${porcentagem}% `)
+      }
+
+      function faturamento(){
+        let Tbruto = Number(prompt("qual o total bruto deste mes?:"))
+        let premiacao = Number(prompt("qual o total de premiaçoes totais deste mes?:"))
+        let presente = Number(prompt("qual o total de presentes deste mes?:"))
+        let comissoes = Number(prompt("qual o total de comissões deste mes?:"))
+
+        let Tmes = Tbruto - premiacao - presente -comissoes
+
+        alert(`O total faturado deste mes menos os gastos sera de : R$${Tmes}`)
+      }
+
+        function frete(){
+          let peso = Number(prompt("qual o peso da encomenda?:"))
+          let distancia = Number(prompt("qual a distandia da encomenda?:"))
+          let volume = Number(prompt("qual o volume da encomenda?:"))
+
+          let frete = 15+(2*peso)+(0.05*distancia)+(10*volume)
+
+          alert(`o frete da sua encomenda vai ser : R$${frete.toFixed(2)}`)
+        }
+
+        function chanceCelular(){
+          let Tvezes = Number(prompt("quantas vezes o candidato usou o celular em meio a palestra?:"))
+
+          let chance = (0.1/(1+500 * Tvezes))* 100
+
+          alert(`As chances do canditado fulano ser aprovado são ${chance} `)
+        }
+
+        function verCarga(){
+          let Pcaminhao = Number(prompt("qual o peso do caminhao somente?:"))
+          let Ptotal = Number(prompt("qual o peso do caminhao já com a carga?:"))
+
+          let Pcarga = Ptotal - Pcaminhao
+
+          alert(`o peso da carga do caminhao é de ${Pcarga}`)
+        }
+
+      function pagamentoDiario(){
+        let salarioM = Number(prompt("qual seu salario mensal?:"))
+        let dias = Number(prompt("quantos dias voce trabalhou este mes?:"))
+
+        let porDia = salarioM / dias
+        let porSemana = porDia * 5
+
+        alert(`voce recebe R$${porDia} por dia\nE recebe R$${porSemana} na semana`)
+
+      }
+
+  function custosMensais(){
+    let custo = Number(prompt("qual o valor de seus custos mensais"))
+    let doacoes =  Number(prompt("qual o valor das doaçoes?:"))
+
+    let restante = custo - doacoes
+
+      alert(`faltam R$${restante} para pagar sua divida de R$${custo}`)
+  }
+
+  function totalLaranjas(){
+let Qinicial = Number(prompt("qual a quantidade inicial de laranjas?:"))
+let Qvendida =  Number(prompt("qual a quantidade de laranjas vendidas?:"))
+
+let Qfinal = Qinicial - Qvendida
+
+alert(`após a venda de ${Qvendida} laranjas, sobraram ${Qfinal}`)
+
+  }
+ 
+  function contagemDev(){
+    let clt = Number(prompt("quantos dev clt sua impresa possui?:"))
+    let pj = Number(prompt("quantos dev pj sua impresa possui?:"))
+    let estagiarios = Number(prompt("quantos dev estagiarios sua impresa possui?:"))
+
+    let quantidade = clt + pj + estagiarios
+
+    alert(`A empresa tem o total de ${quantidade} de devs`)
+  }
+
+  function Pepequeno(){
+
+    let preco = Number(prompt("qual o preço dos calçados:"))
+    let quantidade = Number(prompt("quantas unidades?: "))
+
+    let valor = preco * quantidade
+
+    alert(`a impresa vai recer R$${valor} em vale-trocas`)
+
+  }
+   function calcularPontos(){
+        let vitorias = Number(prompt("numero de vitorias:"))
+        let empates = Number(prompt("numero de empates:"))
+      
+        let pontos = vitorias * 3 + empates 
+        
+        alert(`seu time tem ${pontos} pontos `)
+      }
+
   function testar(){
     let nome = prompt("qual seu nome ?")
     let bocaDoSapo = nome;
@@ -16,16 +140,40 @@ function App() {
 
       let resultado = (n1 + n2 + n3) / 3
 
-      alert(`sua media é : ${resultado.toFixed(1)}`)
+      alert(`sua media é : ${resultado.toFixed(1)}`)  
   }
 
   return (
     <div className="const-app">
       <h1>JavaScript no Ract</h1>
-      <button onClick={testar}>testar</button>
+
+      <h2>exercicios maneiros</h2>
+
+      <button onClick={calcularPontos}>campeonato</button>
+      <button onClick={Pepequeno}>ver vale-troca</button>
+      <button onClick={contagemDev}>funcionarios/dev</button>
+      <br />
+      <button onClick={totalLaranjas}>laranjas vendidas</button>
+      <button onClick={custosMensais}>custos do mes</button>
+      <button onClick={pagamentoDiario}>pagamento diario</button>
+      <br />
+      <button onClick={verCarga}>carga do caminhao</button>
+      <button onClick={chanceCelular}>veja as chances</button>
+      <button onClick={frete}>ver frete</button>
+      <br />
+      <button onClick={faturamento}>ver faturamento</button>
+      <button onClick={mensalBarco}>faturamento barco</button>
+      <button onClick={PlanejarS}>planejamento de shows</button>
+
+      <hr />
+      
+
+
+      <button onClick={testar}>surpresa</button>
       <button onClick={calcularMedia}>Media</button>
       </div>
   )
 }
 
 export default App
+
